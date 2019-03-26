5d63e49d48c2f5c906bed2e7ce2df4b0be5ce319
inemao <inema.orukari@gmail.com>
2019-03-26 12:36:57 -0500

    Initial Commit

---
+++ Set.Graphical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set.Graphical
+//
+//  Created by beckerresearch on 3/26/19.
+//  Copyright © 2019 Washington University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
